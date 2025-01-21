7b79e2239a1674220a22583e6dfc66024c27f8ba
Sreenidhi-G2 <152508973+Sreenidhi-G2@users.noreply.github.com>
2025-01-21 15:28:37 +0530

    Update routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,10 +3,10 @@ const auth = require('../middleware/auth');
 
 const router=require('express').Router();
 
-router.post('/register',userctrl.register)
-router.post('/refresh_token',userctrl.refreshToken)
-router.post('/login',userctrl.login)
-router.get('/logout',userctrl.logout)
-router.get('/infor',auth,userctrl.getUser)
+router.post('register',userctrl.register)
+router.post('refresh_token',userctrl.refreshToken)
+router.post('login',userctrl.login)
+router.get('logout',userctrl.logout)
+router.get('infor',auth,userctrl.getUser)
 
 module.exports=router;
